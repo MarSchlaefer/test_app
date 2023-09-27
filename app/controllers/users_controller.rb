3d4c5942a7c57991a2a9a5f023de588f57b8eb67
Mar Jones <mschlaefer00@gmail.com>
2023-09-27 11:00:07 -0400

    Clean up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
     def index
         @users = User.all
     end
@@ -10,6 +12,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}!"
             redirect_to @user
         else
@@ -18,16 +21,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your info was updated!"
             redirect_to @user
@@ -39,5 +39,9 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
