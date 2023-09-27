3f3d9f64e0297bfb17567b72bd954efcd9f89d51
Mar Jones <mschlaefer00@gmail.com>
2023-09-27 11:52:16 -0400

    Add logged in checks to views.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-        flash.now = "Logged out"
+        flash[:notice] = "Logged out"
         redirect_to root_path
-    end
+      end
 end
