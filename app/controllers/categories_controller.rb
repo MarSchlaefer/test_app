2ca60c6a4e08b8de57c329074f67b56de9b32955
Mar Jones <mschlaefer00@gmail.com>
2023-09-28 11:13:44 -0400

    Add tests and test helpers.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
 
     def index
+        @categories = Category.all
     end
 
     def new
@@ -18,12 +20,21 @@ class CategoriesController < ApplicationController
     end
 
     def show
+        @category = Category.find(params[:id])
     end
 
     private
 
     def category_params
         params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "Only admins can do that"
+            redirect_to categories_path
+        end
+
     end
     
 end
